fa32f57823276ba25dda3c51f4c4669c394177b9
sandeepmukherjee <sandeepmukherjee12@gmail.com>
2025-02-07 21:54:41 +0530

    Line Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import Line from "@/components/line";
 
 export default function Home() {
   return (
       <main className="flex flex-col items-center px-4">
         <Intro/>
+        <Line/>
       </main>
   );
 }
